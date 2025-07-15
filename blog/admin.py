363c599c97bceb6cb7296f5078de92cf94e2bb8a
Cesar Gimenez Lascano <cesargimenez@gmail.com>
2025-07-15 18:44:49 -0300

    mejorar visualizacion del panel

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'fecha_publicacion']
+    ordering = ['-fecha_publicacion']
 
 

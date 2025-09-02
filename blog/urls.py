21d1fd5a1a4ede2e86d879df432336ab08a7de5a
Cesar Gimenez Lascano <cesargimenez@gmail.com>
2025-09-02 17:21:56 -0300

    vistas basadas en clases

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+   path('', views.PostListView.as_view(), name='post_list'),
+    path('post/list/', views.PostListView.as_view(), name='post_list'),
     path('post/create/', views.post_create, name='post_create'),
+    path('post/detail/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/delete/<int:pk>/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,34 @@
+from django.views.generic import ListView, DetailView, DeleteView
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 # Create your views here.
 from .models import Post
 from .forms import PostForm
 
-def post_list(request):
-    busqueda=request.GET.get('busqueda', None)
-    if busqueda:
-        post_list = Post.objects.filter(titulo__icontains=busqueda)
-    else:
-        post_list = Post.objects.all()
-    return render(request, 'blog/post_list.html', context={'posts': post_list})
+# vista de clase
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        busqueda = self.request.GET.get('busqueda', None)
+        if busqueda:
+            queryset = queryset.filter(titulo__icontains=busqueda)
+        return queryset
+
+#vista detalle
+class PostDetailView(DetailView):
+    model = Post
+
+#vista eliminar
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:post_list')
 
 #vista del formulario
-
 def post_create(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
